8ad260f549adba1af9c0625ac98b08f8ee0f8162
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-14 15:56:17 +0530

    Create reverse_the_letters_words_of_dtring.py

---
+++ reverse_the_letters_words_of_dtring.py
@@ -0,0 +1,13 @@
+string=input()
+list_string=string.split()
+string_ans=""
+for i in list_string:
+    string_ans+=i[::-1]+" "
+print(string_ans)
+
+
+"""
+input:The cat is on the mat.
+output:ehT tac si no eht .tam 
+
+"""
