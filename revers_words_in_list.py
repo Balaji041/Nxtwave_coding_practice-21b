4fde7d1309835e8b8ce79f6a6ded657c44e1284e
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-14 15:50:50 +0530

    Create revers_words_in_list.py

---
+++ revers_words_in_list.py
@@ -0,0 +1,9 @@
+string=input()
+lsit_string=string.split()
+print(lsit_string[::-1])
+
+"""
+input:Banana Apple Pomegranate Strawberry Grapes Orange
+output:['Orange', 'Grapes', 'Strawberry', 'Pomegranate', 'Apple', 'Banana']
+
+"""
