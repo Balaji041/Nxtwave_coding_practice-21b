6a710d4cf345ec3bbb94fc90e7cbc5ed09ceee46
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-14 16:33:16 +0530

    Create first_half_number_of_list.py

---
+++ first_half_number_of_list.py
@@ -0,0 +1,12 @@
+list_string=input().split()
+l=len(list_string)/2+0.5
+li=[]
+for i in range(int(l)):
+    li+=[int(list_string[i])]
+print(li)
+
+"""
+input:1 3 9 0 8 2 7
+output:[1, 3, 9, 0]
+
+"""
