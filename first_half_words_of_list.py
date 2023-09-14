e00536889ec37e6391507b6385c1977a789cf987
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-14 16:17:46 +0530

    Create first_half_words_of_list.py

---
+++ first_half_words_of_list.py
@@ -0,0 +1,13 @@
+string=input()
+list_string=string.split()
+l=len(list_string)/2+0.5
+p=[]
+for i in range(int(l)):
+    p+=[list_string[i]]
+print(p)
+
+"""
+input:apple mango orange grapes kiwi
+output:['apple', 'mango', 'orange']
+
+"""
